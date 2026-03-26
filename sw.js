843f58545ee2639cc487aafa83fa9a001f258955
mohammadzhafran04-art <mohammadzhafran04@gmail.com>
2026-03-26 18:24:13 +1030

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,25 +1,8 @@
-const CACHE_NAME = 'hq-859-v1';
-const assets = [
-  '/',
-  '/index.html',
-  '/dashboard.html',
-  '/manifest.json'
-];
-
-// Install Service Worker
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(assets);
-    })
-  );
+// Service Worker Minimalis
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
 });
 
-// Jalankan di latar belakang
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
-  );
+self.addEventListener('fetch', (event) => {
+  event.respondWith(fetch(event.request));
 });
