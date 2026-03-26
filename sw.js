d8ecd7bc14f6dc1cf4e2cfa6005725a81391e7f8
mohammadzhafran04-art <mohammadzhafran04@gmail.com>
2026-03-26 18:32:10 +1030

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,8 +1,35 @@
-// Service Worker Minimalis
+const CACHE_NAME = 'tac-v7-cache-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json'
+];
+
+// Install Service Worker
 self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
   self.skipWaiting();
 });
 
+// Activate & Cleanup Old Caches
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+// Fetch Assets
 self.addEventListener('fetch', (event) => {
-  event.respondWith(fetch(event.request));
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
+  );
 });
